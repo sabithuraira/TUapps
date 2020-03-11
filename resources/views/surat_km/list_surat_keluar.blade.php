0e6b62fc6f49269ece99c754feb8053c0ca9f07f
sabit_huraira <sabitzhabit@gmail.com>
2020-03-11 11:59:43 +0700

    fix tampilan surat KM

--- resources/views/surat_km/list_surat_keluar.blade.php
+++ resources/views/surat_km/list_surat_keluar.blade.php
@@ -9,10 +9,8 @@
                 <thead>
                     <tr>
                         <th>{{ $surat_keluar[0]->attributes()['nomor_urut'] }}</th>
-                        <th>{{ $surat_keluar[0]->attributes()['nomor_petunjuk'] }}</th>
-                        <th>{{ $surat_keluar[0]->attributes()['alamat'] }}</th>
+                        <th>Isi</th>
                         <th>{{ $surat_keluar[0]->attributes()['tanggal'] }}</th>
-                        <th>{{ $surat_keluar[0]->attributes()['perihal'] }}</th>
                         <th class="text-center" colspan="2">Action</th>
                     </tr>
                 </thead>
@@ -21,14 +19,14 @@
                     <tr>
                         <td>{{$data['nomor_urut']}}</td>
                         <td>
+                            <h6 class="margin-0">Perihal: {{$data['perihal']}}</h6>
                             @if(array_key_exists($data['nomor_petunjuk'], $data->listPetunjuk))
-                                {{ $data->listPetunjuk[$data['nomor_petunjuk']] }} - {{ $data['nomor_petunjuk'] }}
+                                <p class="text-muted">{{ $data->listPetunjuk[$data['nomor_petunjuk']] }} - {{ $data['nomor_petunjuk'] }}</p>
                             @endif
+                            <span>Alamat: {{$data['alamat']}}</span>
+
                         </td>
-                        <td>{{$data['alamat']}}</td>
                         <td>{{$data['tanggal']}}</td>
-                        <td>{{$data['perihal']}}</td>
-                        
                         <td class="text-center"><a href="{{action('SuratKmController@edit', $data['id'])}}"><i class="icon-pencil text-info"></i></a></td>
                         <td class="text-center">
                         <form action="{{action('SuratKmController@destroy', $data['id'])}}" method="post">
