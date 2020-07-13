88c0f9a1eafac115dff5502a7c61e9633e3097f7
sabit_huraira <sabitzhabit@gmail.com>
2020-07-13 13:05:51 +0700

    AUTH - delete some

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,35 +44,7 @@ class HomeController extends Controller
     }
 
     public function hai(){
-        $service_url    = 'https://backoffice.bps.go.id/simpeg_api/bps16';
-        $curl           = curl_init($service_url);
-        $curl_post_data = array(
-            "apiKey" => '4vl8i/WeNeRlRxM4KDk93VqdT0/LZ9g+GBITo+OiHVs=',
-            "kategori"=> 'view_pegawai',
-            "nip" => '340055914',
-        );
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-        $curl_response = json_decode(curl_exec($curl));
-
-        if(!isset($curl_response->error)){
-            if($curl_response->total==0){
-                // $is_cookie = false;
-            }
-            else{
-                $c_model = $curl_response->pegawai[0];
-                print_r($c_model->niplama);
-                print_r($c_model->nmstjab);
-                print_r($c_model->kdstjab);
-                print_r($c_model->nmstkerja);
-                die();
-            }
-        }
-        else{
-            print_r($curl_response->error);
-        }
-        // return view('hai');
+        return view('hai');
     }
 
     public function guest(){
