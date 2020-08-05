8b223213f2f6ea418134dce80feeb075b1a40b90
sabit_huraira <sabitzhabit@gmail.com>
2020-08-05 16:29:12 +0700

    SP2020 - update telegram

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -10,7 +10,7 @@ class TelegramController extends Controller
 {
     public function sp2020(Request $request)
     { 
-        $pesan = json_encode("halo dong");
+        $pesan = "Haloo, test webhooks <code>dicoffeean.com</code>.&parse_mode=HTML";
         $API = "https://api.telegram.org/bot".env('TELEGRAM_TOKEN')."/sendmessage?chat_id=441016770&text=$pesan";
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -19,5 +19,14 @@ class TelegramController extends Controller
         $result = curl_exec($ch);
         curl_close($ch);
         return $result;
+
+        // $apiURL = "https://api.telegram.org/bot".env('TELEGRAM_TOKEN');
+        // $update = json_decode(file_get_contents("php://input"), TRUE);
+        // $chatID = $update["message"]["chat"]["id"];
+        // $message = $update["message"]["text"];
+        
+        // if (strpos($message, "/start") === 0) {
+        //     file_get_contents($apiURL."/sendmessage?chat_id=".$chatID."&text=Haloo, test webhooks <code>dicoffeean.com</code>.&parse_mode=HTML");
+        // }
     }
 }
